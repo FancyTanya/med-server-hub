4f7c22324e06c53964f69830bf3c784f9c450c8e
tanya <tetyana.gromova@data-ox.com>
2023-09-25 13:14:23 +0300

    Fixed RequestMapping

--- src/main/java/com/github/mediservehub/infrastructure/CustomUserDetailsService.java
+++ src/main/java/com/github/mediservehub/infrastructure/CustomUserDetailsService.java
@@ -1,91 +1,93 @@
-//package com.github.mediservehub.infrastructure;
-//
-//import com.github.mediservehub.domain.user.User;
-//import com.github.mediservehub.domain.user.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Arrays;
-//import java.util.Collection;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return new CustomUserDetails(userRepository.findUserByUsername(username));
-//    }
-//
-//    public static class CustomUserDetails implements UserDetails {
-//
-//        private final SimpleGrantedAuthority USER_DOCTOR = new SimpleGrantedAuthority("ROLE_DOCTOR");
-//
-//        private final SimpleGrantedAuthority USER_MANAGER = new SimpleGrantedAuthority("ROLE_MANAGER");
-//
-//        private final Collection<? extends GrantedAuthority> ROLES_USER =
-//                Arrays.asList(USER_DOCTOR, USER_MANAGER);
-//
-//        private final Long id;
-//
-//        private final String username;
-//
-//        private final String password;
-//
-//        private final Collection<? extends GrantedAuthority> roles;
-//
-//        public CustomUserDetails(final User user) {
-//            this.id = user.getId();
-//            this.username = user.getUsername();
-//            this.password = user.getPassword();
-//            roles = ROLES_USER;
-//        }
-//
-//        public Long getId() {
-//            return id;
-//        }
-//
-//        @Override
-//        public Collection<? extends GrantedAuthority> getAuthorities() {
-//            return roles;
-//        }
-//
-//        @Override
-//        public String getPassword() {
-//            return password;
-//        }
-//
-//        @Override
-//        public String getUsername() {
-//            return username;
-//        }
-//
-//        @Override
-//        public boolean isAccountNonExpired() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isAccountNonLocked() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isCredentialsNonExpired() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isEnabled() {
-//            return true;
-//        }
-//
-//    }
-//}
+package com.github.mediservehub.infrastructure;
+
+import com.github.mediservehub.domain.user.User;
+import com.github.mediservehub.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new CustomUserDetails(userRepository.findUserByUsername(username).orElseThrow
+                (() -> new AuthenticationServiceException("Invalid username")));
+    }
+
+    public static class CustomUserDetails implements UserDetails {
+
+        private final SimpleGrantedAuthority USER_DOCTOR = new SimpleGrantedAuthority("ROLE_DOCTOR");
+
+        private final SimpleGrantedAuthority USER_MANAGER = new SimpleGrantedAuthority("ROLE_MANAGER");
+
+        private final Collection<? extends GrantedAuthority> ROLES_USER =
+                Arrays.asList(USER_DOCTOR, USER_MANAGER);
+
+        private final Long id;
+
+        private final String username;
+
+        private final String password;
+
+        private final Collection<? extends GrantedAuthority> roles;
+
+        public CustomUserDetails(final User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.password = user.getPassword();
+            roles = ROLES_USER;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return roles;
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+    }
+}
--- src/main/java/com/github/mediservehub/integration/MainController.java
+++ src/main/java/com/github/mediservehub/integration/MainController.java
@@ -1,61 +1,57 @@
-//package com.github.mediservehub.integration;
-//
-//import com.github.mediservehub.domain.resourse.*;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.rest.webmvc.PersistentEntityResource;
-//import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-//import org.springframework.data.rest.webmvc.RepositoryRestController;
-//import org.springframework.data.web.PageableDefault;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RepositoryRestController
-//@RequiredArgsConstructor
-//@RequestMapping(value = "/start")
-//public class MainController {
-//
-//    private final FoodStorageRepository storageRepository;
-//    private final PatientRepository patientRepository;
-//    private final StockRepository stockRepository;
-//
-//    private Food food;
-//
-//    @RequestMapping(value = "/foodStorages", method = RequestMethod.GET)
-//    @PreAuthorize("hasRole('ROLE_MANAGER')")
-////    @ResponseBody
-//    public ResponseEntity<List<Food>> getFoodStorage(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable) {
-////                                                   PersistentEntityResourceAssembler asm) {
-//        Page<FoodStorage> foodStorages = storageRepository.findAll(pageable);
-//
-//        List<Food> foods = new ArrayList<>();
-//        foodStorages.stream().forEach(el -> foods.addAll(el.getFoodList()));
-//
-////        return asm.toFullResource(foods);
-//        return new ResponseEntity<>(foods, HttpStatus.OK);
-//    }
-//
-////    @RequestMapping(value = "/patients", method = RequestMethod.GET)
-////    @PreAuthorize("hasRole('ROLE_DOCTOR')")
-////    @ResponseBody
-////    public PersistentEntityResource getPatients(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable,
-////                                                PersistentEntityResourceAssembler asm) {
-////        return asm.toFullResource(patientRepository.findAll(pageable));
-////    }
-////
-////    @RequestMapping(value = "/stocks", method = RequestMethod.GET)
-////    @PreAuthorize("hasAnyRole('ROLE_DOCTOR', 'ROLE_MANAGER')")
-////    @ResponseBody
-////    public PersistentEntityResource getStock(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable,
-////                                             PersistentEntityResourceAssembler asm) {
-////        return asm.toFullResource(stockRepository.findAll(pageable));
-////    }
-//}
+package com.github.mediservehub.integration;
+
+import com.github.mediservehub.domain.resourse.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RepositoryRestController
+@RequiredArgsConstructor
+public class MainController {
+
+    private final FoodStorageRepository storageRepository;
+    private final PatientRepository patientRepository;
+    private final StockRepository stockRepository;
+
+
+    @GetMapping(value = "start/foodStorages")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @ResponseBody
+    public PersistentEntityResource getFoodStorage(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable,
+                                                   PersistentEntityResourceAssembler asm) {
+        Page<FoodStorage> foodStorages = storageRepository.findAll(pageable);
+
+        List<Food> foods = new ArrayList<>();
+        foodStorages.stream().forEach(el -> foods.addAll(el.getFoodList()));
+
+        return asm.toFullResource(foods);
+    }
+
+    @GetMapping(value = "start/patients")
+    @PreAuthorize("hasRole('ROLE_DOCTOR')")
+    @ResponseBody
+    public PersistentEntityResource getPatients(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable,
+                                                PersistentEntityResourceAssembler asm) {
+        return asm.toFullResource(patientRepository.findAll(pageable));
+    }
+
+    @GetMapping(value = "start/stocks")
+    @PreAuthorize("hasAnyRole('ROLE_DOCTOR', 'ROLE_MANAGER')")
+    @ResponseBody
+    public PersistentEntityResource getStock(@PageableDefault(size = 20, page = 0, sort = "id") Pageable pageable,
+                                             PersistentEntityResourceAssembler asm) {
+        return asm.toFullResource(stockRepository.findAll(pageable));
+    }
+}
